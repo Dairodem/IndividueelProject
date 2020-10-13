8559bfdccf2c482187d440a4124c3512e1de4fca
demeulemeester dairo <dairo_demeulemeester@hotmail.com>
2020-10-13 15:01:59 +0200

    dinsdag 13/10

--- IndividueelProject/MagazijnWindow.xaml.cs
+++ IndividueelProject/MagazijnWindow.xaml.cs
@@ -48,10 +48,84 @@ namespace IndividueelProject
 
     public partial class MagazijnWindow : Window
     {
+        string[] overzichtArr = new string[] { "Stock","Klanten","Leveranciers" };
+        Dictionary<string, string> sortDict = new Dictionary<string, string>()
+        {
+            {"Id","Id" },
+            {"Alfabetisch A>Z","alfaUp" },
+            {"Alfabetisch Z>A","alfaDown" },
+        };
+
         public MagazijnWindow()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CbxOverzicht.ItemsSource = overzichtArr;
+            CbxOverzicht.SelectedIndex = 0;
+            
+            CbxSort.ItemsSource = sortDict.Keys;
+            CbxSort.SelectedIndex = 0;
+        }
+
+        private void CbxOverzicht_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (MagazijnEntities ctx = new MagazijnEntities())
+            {
+                
+                switch (CbxOverzicht.SelectedItem.ToString())
+                {
+                    case "Stock":
+                        //kolom namen veranderen
+                        colID.DisplayMemberBinding = new Binding("ps.p.Id");
+                        col1.DisplayMemberBinding = new Binding("Naam");
+                        col1.Header = "Cat.";
+                        col2.DisplayMemberBinding =  new Binding("ps.p.Naam");
+                        col2.Header = "Naam";
+                        col3.DisplayMemberBinding = new Binding("ps.s.Aantal");
+                        col3.Header = "Aantal";
+                        col4.DisplayMemberBinding = new Binding("ps.p.Inkoopprijs");
+                        col4.Header = "Inkoopprijs";
+                        col5.DisplayMemberBinding = new Binding("x");
+                        col6.DisplayMemberBinding = new Binding("x");
+
+                        var productList = ctx.Products.Join(
+                            ctx.Stocks,
+                            p => p.Id,
+                            s => s.IdProduct,
+                            (p, s) => new {p,s }).Join(
+                            ctx.Subcategories,
+                            ps => ps.p.IdSubcategorie,
+                            c => c.Id,
+                            (ps , c) => new {ps, c.Naam }).ToList();
+
+                        LvOverzicht.ItemsSource = productList;
+                        break;
+                    case "Klanten":
+                        //kolom namen veranderen
+                        colID.DisplayMemberBinding = new Binding("Id");
+                        col1.DisplayMemberBinding = new Binding("Bedrijf");
+                        col1.Header = "Bedrijf";
+                        col2.DisplayMemberBinding = new Binding("Telefoonnummer");
+                        col2.Header = "Telefoonnummer";
+                        col3.DisplayMemberBinding = new Binding("AangemaaktOp");
+                        col3.Header = "Aangemaakt op";
+                        LvOverzicht.ItemsSource = ctx.Klants.Select(k => k).ToList();
+                        break;
+                    case "Leveranciers":
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void CbxSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
